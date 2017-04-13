038f111d5c4c519015c6724ca10d7cfd483e9d46
Marius <m.motea@oberthur.com>
2017-04-13 14:21:53 +0300

    beta sketches for tap switch and dimmer switch

--- HueBridge/www/sensors.php
+++ HueBridge/www/sensors.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                 'name' => $row_sensors['name'],
                 'type' => $row_sensors['type'],
                 'modelid' => $row_sensors['modelid'],
-                'manufacturername' => $row_sensors['manufacturername'],
+                'manufacturername' => $row_sensors['manufacturername']
             );
             if (!empty($row_sensors['swversion'])) {
                 $sensors_array[$row_sensors['id']]['swversion'] = $row_sensors['swversion'];
@@ -37,8 +37,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             }
         }
     } elseif (is_numeric($url['4'])) {
-        $query_sensor = mysqli_query($con, "SELECT * FROM sensors WHERE id = '".$url['4']."';");
-        $row_sensor = mysqli_fetch_assoc($query_sensor);
+        $query_sensor = mysqli_query($con, "SELECT * FROM sensors WHERE id = '" . $url['4'] . "';");
+        $row_sensor   = mysqli_fetch_assoc($query_sensor);
         $output_array = array(
             'type' => $row_sensor['type'],
             'state' => json_decode($row_sensor['state']),
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             'modelid' => $row_sensor['modelid'],
             'manufacturername' => $row_sensor['manufacturername'],
             'swversion' => $row_sensor['swversion'],
-            'recycle' => (bool) $row_sensor['recycle'],
+            'recycle' => (bool) $row_sensor['recycle']
         );
         if (!empty($row_sensors['uniqueid'])) {
             $output_array['uniqueid'] = $row_sensor['uniqueid'];
@@ -59,79 +59,58 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     } else {
         $query_new_sensors = mysqli_query($con, 'SELECT id, name FROM sensors WHERE new = 1;');
         while ($row_new_sensors = mysqli_fetch_assoc($query_new_sensors)) {
-            $output_array[$row_new_sensors['id']] = array('name' => $row_new_sensors['name']);
+            $output_array[$row_new_sensors['id']] = array(
+                'name' => $row_new_sensors['name']
+            );
         }
         $output_array['lastscan'] = gmdate("Y-m-d\TH:i:s");
-        $query_remove_new_flag = mysqli_query($con, 'UPDATE sensors SET new = 0 WHERE new = 1;');
+        $query_remove_new_flag    = mysqli_query($con, 'UPDATE sensors SET new = 0 WHERE new = 1;');
     }
 } elseif ($_SERVER['REQUEST_METHOD'] == 'PUT') {
     $data = json_decode(file_get_contents('php://input'), false);
-    error_log('SENSORS PUT: '.json_encode($data));
+    error_log('SENSORS PUT: ' . json_encode($data));
     $update_string = 'UPDATE sensors SET ';
-    foreach ($data as $key => $value) {
-        $output_array[] = array(
-            'success' => array(
-                $url['3'].'/'.$url['4'].'/'.$url['5'].'/'.$key => $value,
-            ),
-        );
-        if ($key == 'on') {
-            $key = 'state';
+    if ($url[5] == 'state' || $url[5] == 'config') {
+        $update_string .= $url[5] . " = '" . json_encode($data) . "'";
+    } else {
+        foreach ($data as $key => $value) {
+
+            if (is_array($value)) {
+                $value = json_encode($value);
+            }
+            error_log($key . '|' . $value);
+            $update_string .= $key . " = '" . $value . "',";
         }
-        if (is_array($value)) {
-            $value = json_encode($value);
-        }
-        error_log($key.'|'.$value);
-        $update_string .= $key." = '".$value."',";
     }
     $update_string = rtrim($update_string, ',');
-    $update_string .= ' WHERE id = '.$url['4'];
+
+    $update_string .= ' WHERE id = ' . $url['4'];
     $update_sensors = mysqli_query($con, $update_string);
+
+    foreach ($data as $key => $value) {
+        $url_response   = implode('/', array_slice($url, 3));
+        $output_array[] = array(
+            'success' => array(
+                '/' . $url_response . '/' . $key => $value
+            )
+        );
+    }
     error_log($update_string);
 } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data = json_decode(file_get_contents('php://input'), true);
-    error_log('SENSORS POST:'.json_encode($data));
+    error_log('SENSORS POST:' . json_encode($data));
     if (empty($data)) {
         $output_array[] = array(
             'success' => array(
-                '/sensors' => 'Searching for new devices',
-            ),
+                '/sensors' => 'Searching for new devices'
+            )
         );
-        $network_ips = shell_exec("nmap  $gateway/24 -p81 --open -n | grep report | cut -d ' ' -f5");
-        $ips_array = explode("\n", $network_ips);
-        foreach ($ips_array as $ip) {
-            $url = "http://$ip/detect";
-            $ch = curl_init();
-
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
-
-            $result = curl_exec($ch);
-            if (!empty($result)) {
-                $data = json_decode($result, true);
-                if (is_array($data) && isset($data['hue'])) {
-                    error_log("$ip is a hue ".$data['hue']);
-                    $new_light = mysqli_query($con, "SELECT ip FROM sensors WHERE uniqueid = '".$data['mac']."' LIMIT 1;");
-                    $row_cnt = $new_light->num_rows;
-                    $row_new_sensor = mysqli_fetch_assoc($new_sensor);
-                    if ($row_cnt > 0) {
-                        error_log('this is already present');
-                    } else {
-                        if ($data['type'] == 'tap' || $data['type'] == 'dimer') {
-                            mysqli_query($con, "INSERT INTO `lights`(`bri`, `ct`, `colormode`, `type`, `name`, `uniqueid`, `modelid`, `swversion`, `ip`, `strip_light_nr`) VALUES ('200', '461', 'ct', 'Extended color light','Hue ".$data['type'].' '.$data['hue']." Light $i','".$data['mac'].'-'.$i."','".(($data['hue'] == 'strip') ? 'LST001' : 'LCT001')."','66009461', '$ip', '$i');");
-                            error_log("INSERT INTO `lights`(`bri`, `ct`, `colormode`, `type`, `name`, `uniqueid`, `modelid`, `swversion`, `ip`, `strip_light_nr`) VALUES ('200', '461', 'ct', 'Extended color light','Hue ".$data['type'].' '.$data['hue']." Light $i','".$data['mac'].'-'.$i."','".(($data['hue'] == 'strip') ? 'LST001' : 'LCT001')."','66009461', '$ip', '$i');");
-                        }
-                    }
-                }
-                curl_close($ch);
-            }
-        }
     } else {
-        mysqli_query($con, "INSERT INTO `sensors`(`type`, `state`, `name`, `modelid`, `manufacturername`, `uniqueid`, `swversion`, `recycle`) VALUES ('".$data['type']."','".((empty($data['state'])) ? '' : json_encode($data['state']))."','".$data['name']."','".$data['modelid']."','".$data['manufacturername']."','".$data['uniqueid']."','".$data['swversion']."',".((isset($data['recycle'])) ? 1 : 0).');');
+        mysqli_query($con, "INSERT INTO `sensors`(`type`, `state`, `name`, `modelid`, `manufacturername`, `uniqueid`, `swversion`, `recycle`) VALUES ('" . $data['type'] . "','" . ((empty($data['state'])) ? '' : json_encode($data['state'])) . "','" . $data['name'] . "','" . $data['modelid'] . "','" . $data['manufacturername'] . "','" . $data['uniqueid'] . "','" . $data['swversion'] . "'," . ((isset($data['recycle'])) ? 1 : 0) . ');');
         $output_array[] = array(
             'success' => array(
-                'id' => (string) mysqli_insert_id($con),
-            ),
+                'id' => (string) mysqli_insert_id($con)
+            )
         );
     }
 }
---
+++ Sensors/HueDimmerSwitch/HueDimmerSwitch.ino
@@ -0,0 +1,143 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+extern "C" {
+#include "gpio.h"
+#include "user_interface.h"
+}
+
+const char* ssid = "MikroTik";
+const char* password = "nustiuceparola";
+
+#define button1_pin 15
+#define button2_pin 4
+#define button3_pin 5
+#define button4_pin 12
+
+const char* switchType = "ZLLSwitch";
+
+const char* bridgeIp = "192.168.10.200";
+
+IPAddress strip_ip ( 192,  168,   10,  96);
+IPAddress gateway_ip ( 192,  168,   10,   1);
+IPAddress subnet_mask(255, 255, 255,   0);
+
+int counter;
+byte mac[6];
+
+void goingToSleep() {
+  yield();
+  delay(100);
+  ESP.deepSleep(0);
+  yield();
+}
+
+String macToStr(const uint8_t* mac) {
+  String result;
+  for (int i = 0; i < 6; ++i) {
+    result += String(mac[i], 16);
+    if (i < 5)
+      result += ':';
+  }
+  return result;
+}
+
+void sendHttpRequest(int button) {
+  WiFiClient client;
+  String url = "/switch?mac=" + macToStr(mac) + "&button=" + button;
+  client.connect(bridgeIp, 80);
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + bridgeIp + "\r\n" +
+               "Connection: close\r\n\r\n");
+}
+
+void setup() {
+  pinMode(15, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(12, INPUT);
+  pinMode(16, OUTPUT);
+  digitalWrite(16, LOW);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  WiFi.config(strip_ip, gateway_ip, subnet_mask);
+  WiFi.macAddress(mac);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(50);
+  }
+
+  ArduinoOTA.begin();
+
+  rst_info *rinfo;
+  rinfo = ESP.getResetInfoPtr();
+
+  if ((*rinfo).reason != REASON_DEEP_SLEEP_AWAKE) {
+
+    WiFiClient client;
+    client.connect(bridgeIp, 80);
+
+    //register device
+    String url = "/switches";
+    url += "?devicetype=" + (String)switchType;
+    url += "&mac=" + macToStr(mac);
+
+    //###Registering device
+    client.connect(bridgeIp, 80);
+    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+                 "Host: " + bridgeIp + "\r\n" +
+                 "Connection: close\r\n\r\n");
+  }
+
+
+}
+
+void loop() {
+  ArduinoOTA.handle();
+  delay(1);
+
+  if (digitalRead(button1_pin) == HIGH) {
+    sendHttpRequest(1000);
+    int i;
+    while (digitalRead(button1_pin) == HIGH & i < 20) {
+      i++;
+      delay(50);
+    }
+    counter = 0;
+  }
+  if (digitalRead(button2_pin) == HIGH) {
+    sendHttpRequest(2000);
+    int i;
+    while (digitalRead(button2_pin) == HIGH & i < 20) {
+      i++;
+      delay(50);
+    }
+    counter = 0;
+  }
+  if (digitalRead(button3_pin) == HIGH) {
+    sendHttpRequest(3000);
+    int i;
+    while (digitalRead(button3_pin) == HIGH & i < 20) {
+      i++;
+      delay(50);
+    }
+    counter = 0;
+  }
+  if (digitalRead(button4_pin) == HIGH) {
+    sendHttpRequest(4000);
+    int i;
+    while (digitalRead(button4_pin) == HIGH & i < 20) {
+      i++;
+      delay(50);
+    }
+    counter = 0;
+  }
+  if (counter == 5000) {
+    goingToSleep();
+  } else {
+    counter++;
+  }
+}
+
