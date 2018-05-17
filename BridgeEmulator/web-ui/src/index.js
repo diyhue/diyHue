5dc256228027b1dcb66b00f815be9d4179664980
Federico Zivolo <fzivolo@quid.com>
2018-05-17 09:49:09 +0200

    fix: throttle API calls and minor fixes

--- BridgeEmulator/web-ui/src/index.js
+++ BridgeEmulator/web-ui/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import { injectGlobal } from "react-emotion";
 import { compose, withState, lifecycle, withProps } from "recompose";
+import _throttle from "lodash/fp/throttle";
 import App from "./App";
 
 injectGlobal`
@@ -11,6 +12,9 @@ injectGlobal`
 `;
 
 const API_KEY = window.config.API_KEY;
+const THROTTLE_WAIT = 1000; // 1 second
+
+const throttle = _throttle(THROTTLE_WAIT);
 
 const enhance = compose(
   withState("groups", "setGroups", {}),
@@ -19,13 +23,16 @@ const enhance = compose(
   // entity may be a light or a room, they have an extra `id` property
   // the second argument is the new value selected by the user
   withProps({
-    onColorTemperatureChange: (entity, temp) => console.log(entity.id, temp),
-    onColorChange: (entity, color) => console.log(entity.id, color),
-    onBrightnessChange: (entity, bri) => console.log(entity.id, bri),
-    onStateChange: (entity, state) =>
-      console.log(entity.id, entity.type, state),
+    onColorTemperatureChange: throttle((entity, temp) =>
+      console.log(entity.id, temp)
+    ),
+    onColorChange: throttle((entity, color) => console.log(entity.id, color)),
+    onBrightnessChange: throttle((entity, bri) => console.log(entity.id, bri)),
+    onStateChange: throttle((entity, state) =>
+      console.log(entity.id, entity.type, state)
+    ),
     // this should trigger a state change on ALL the available lights
-    onGlobalStateChange: state => console.log("all the lights", state)
+    onGlobalStateChange: throttle(state => console.log("all the lights", state))
   }),
   lifecycle({
     async componentDidMount() {
