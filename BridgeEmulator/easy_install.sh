6d79b06f6cb581458f68167b5efaf5ad977ac378
Marius <marius.motea@gmail.com>
2018-07-21 11:24:04 +0300

    dinamyc interface name in easy_install

--- BridgeEmulator/easy_install.sh
+++ BridgeEmulator/easy_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mac=`cat /sys/class/net/eth0/address`
+mac=`cat /sys/class/net/$(ip addr show | awk '/inet.*brd/{print $NF}')/address`
 serial="${mac:0:2}${mac:3:2}${mac:6:2}fffe${mac:9:2}${mac:12:2}${mac:15:2}"
 apt install -y git nmap python3 python3-requests python3-ws4py nginx
 cd /tmp
@@ -8,7 +8,7 @@ mkdir /opt/hue-emulator
 cd diyHue/BridgeEmulator
 dec_serial=`python -c "print(int(\"$serial\", 16))"`
 openssl req -new  -config openssl.conf  -nodes -x509 -newkey  ec -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve   -subj "/C=NL/O=Philips Hue/CN=$serial" -keyout private.key -out public.crt -set_serial $dec_serial
-cp -r web-ui functions cert.pem HueEmulator3.py coap-client-linux config.json private.key public.crt /opt/hue-emulator/
+cp -r web-ui functions HueEmulator3.py coap-client-linux config.json private.key public.crt /opt/hue-emulator/
 cp entertainment-`uname -m` /opt/hue-emulator/entertainment-srv
 cp hue-emulator.service /lib/systemd/system/
 chmod 644 /lib/systemd/system/hue-emulator.service
