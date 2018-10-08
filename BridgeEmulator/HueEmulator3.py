9a1f77fb60ce0fe0e8aadbc6fea48536b6a83103
Motea Marius <marius.motea@gmail.com>
2018-10-08 20:58:40 +0300

    Switch Milight to hs mode to fix bug #405

--- BridgeEmulator/HueEmulator3.py
+++ BridgeEmulator/HueEmulator3.py
@@ -741,8 +741,9 @@ def syncWithLights(): #update Hue Bridge lights states
                         bridge_config["lights"][light]["state"]["colormode"] = "ct"
                         bridge_config["lights"][light]["state"]["ct"] = light_data["color_temp"] * 1.6
                     elif "bulb_mode" in light_data and light_data["bulb_mode"] == "color":
-                        bridge_config["lights"][light]["state"]["colormode"] = "xy"
-                        bridge_config["lights"][light]["state"]["xy"] = convert_rgb_xy(light_data["color"]["r"], light_data["color"]["g"], light_data["color"]["b"])
+                        bridge_config["lights"][light]["state"]["colormode"] = "hs"
+                        bridge_config["lights"][light]["state"]["hue"] = light_data["hue"] * 180)
+                        bridge_config["lights"][light]["state"]["sat"] = int(light_data["saturation"] * 2.54))
                 elif bridge_config["lights_address"][light]["protocol"] == "domoticz": #domoticz protocol
                     light_data = json.loads(sendRequest("http://" + bridge_config["lights_address"][light]["ip"] + "/json.htm?type=devices&rid=" + bridge_config["lights_address"][light]["light_id"], "GET", "{}"))
                     if light_data["result"][0]["Status"] == "Off":
