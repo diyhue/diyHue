f69567641ffae55d1571f90f18ea44e9f4a04e82
Motea Marius <marius.motea@gmail.com>
2018-10-31 16:06:27 +0200

    Entire code was refactored

--- Sensors/HueDimmerSwitch/HueDimmerSwitch.ino
+++ Sensors/HueDimmerSwitch/HueDimmerSwitch.ino
@@ -7,31 +7,35 @@ extern "C" {
 #include "user_interface.h"
 }
 
-#define button1_pin 1
+#define button1_pin 4
 #define button2_pin 3
-#define button3_pin 5
-#define button4_pin 4
+#define button3_pin 13
+#define button4_pin 14
 
-const char* ssid = "MikroTik";
-const char* password = "nustiuceparola";
-const char* switchType = "ZLLSwitch";
-const char* bridge_user = "9f552609847c0dd1f372fee9b76330035b7a3bb1";
-
-//Set bridge ip or ip of every light controlled by this switch
-const char* bridgeIp[] = {"192.168.10.200"};
+const char* ssid = "WiFi name"; // replace with your wifi name
+const char* password = "WiFi password"; // replace with your wifi password
+const char* bridgeIp = "192.168.xxx.xxx"; //replace with the hue emulator device ip
 
 //static ip configuration is necessary to minimize bootup time from deep sleep
 IPAddress strip_ip ( 192,  168,   0,  95); // choose an unique IP Adress
 IPAddress gateway_ip ( 192,  168,   0,   1); // Router IP
 IPAddress subnet_mask(255, 255, 255,   0);
 
-int counter;
+// Existing firmware can be updated Over the Air (OTA) by enabling Arduino ota with ON and OFF buttons placed in same time (led will flash 5 times)
+
+/// END USER CONFIG
+const char* switchType = "ZLLSwitch";
+bool otaEnabled = false;
+
+
+
 byte mac[6];
+uint8_t button;
 
 void goingToSleep() {
   yield();
   delay(100);
-  ESP.deepSleep(0);
+  ESP.deepSleep(100);
   yield();
 }
 
@@ -47,26 +51,46 @@ String macToStr(const uint8_t* mac) {
 
 void sendHttpRequest(int button) {
   WiFiClient client;
-  for (int i = 0; i < sizeof(bridgeIp) / 4; i++) {
-    String url = "/switch?mac=" + macToStr(mac) + "&button=" + button;
-    client.connect(bridgeIp[i], 80);
-    client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + bridgeIp[0] + "\r\n" +
-                 "Connection: close\r\n\r\n");
+  String url = "/switch?mac=" + macToStr(mac) + "&button=" + button;
+  client.connect(bridgeIp, 80);
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + bridgeIp + "\r\n" +
+               "Connection: close\r\n\r\n");
+}
+
+
+void readButtons() {
+  if (digitalRead(button1_pin) == HIGH) {
+    button = 1;
+  } else if (digitalRead(button2_pin) == HIGH) {
+    button = 2;
+  } else if (digitalRead(button3_pin) == HIGH) {
+    button = 3;
+  } else if (digitalRead(button4_pin) == HIGH) {
+    button = 4;
+  }
+}
+
+
+void blinkLed(uint8_t count) {
+  for (uint8_t i = 0; i <= count; i++) {
+    digitalWrite(2, LOW);
+    delay(100);
+    digitalWrite(2, HIGH);
+    delay(100);
   }
 }
 
 void setup() {
-  pinMode(16, OUTPUT);
   pinMode(button1_pin, INPUT);
   pinMode(button2_pin, INPUT);
   pinMode(button3_pin, INPUT);
   pinMode(button4_pin, INPUT);
-  digitalWrite(button1_pin, LOW);
-  digitalWrite(button2_pin, LOW);
-  digitalWrite(button3_pin, LOW);
-  digitalWrite(button4_pin, LOW);
-  digitalWrite(16, LOW);
+  pinMode(2, OUTPUT);
+  digitalWrite(2, HIGH);
+
+  readButtons();
+
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -77,15 +101,13 @@ void setup() {
     delay(50);
   }
 
-  ArduinoOTA.begin();
-
   rst_info *rinfo;
   rinfo = ESP.getResetInfoPtr();
 
   if ((*rinfo).reason != REASON_DEEP_SLEEP_AWAKE) {
 
     WiFiClient client;
-    client.connect(bridgeIp[0], 80);
+    client.connect(bridgeIp, 80);
 
     //register device
     String url = "/switch";
@@ -93,65 +115,48 @@ void setup() {
     url += "&mac=" + macToStr(mac);
 
     //###Registering device
-    client.connect(bridgeIp[0], 80);
+    client.connect(bridgeIp, 80);
     client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-                 "Host: " + bridgeIp[0] + "\r\n" +
+                 "Host: " + bridgeIp + "\r\n" +
                  "Connection: close\r\n\r\n");
   }
 
 }
 
 void loop() {
-  ArduinoOTA.handle();
-  delay(1);
 
-  if (digitalRead(button1_pin) == HIGH) {
-    sendHttpRequest(1000);
-    counter = 0;
-    int i = 0;
-    while (digitalRead(button1_pin) == HIGH && i < 30) {
-      delay(20);
-      i++;
+  if (button == 1) {
+    if (digitalRead(button4_pin) == HIGH) {
+      ArduinoOTA.begin();
+      blinkLed(5);
+      otaEnabled = true;
+      button = 5;
+    } else {
+      sendHttpRequest(1000);
     }
   }
-  if (digitalRead(button2_pin) == HIGH) {
-    counter = 0;
-    int i = 0;
-    while (digitalRead(button2_pin) == HIGH && i < 30) {
-      delay(20);
-      i++;
-    }
-    if (i < 30) {
-      sendHttpRequest(2000);
-    } else {
+  else if (button == 2) {
+    sendHttpRequest(2000);
+    delay(300);
+    while (digitalRead(button2_pin) == HIGH) {
+      delay(900);
       sendHttpRequest(2001);
     }
   }
-  if (digitalRead(button3_pin) == HIGH) {
-    counter = 0;
-    int i = 0;
-    while (digitalRead(button3_pin) == HIGH && i < 30) {
-      delay(20);
-      i++;
-    }
-    if (i < 30) {
-      sendHttpRequest(3000);
-    } else {
+  else if (button == 3) {
+    sendHttpRequest(3000);
+    delay(300);
+    while (digitalRead(button3_pin) == HIGH) {
+      delay(900);
       sendHttpRequest(3001);
     }
   }
-  if (digitalRead(button4_pin) == HIGH) {
+  else if (button == 4) {
     sendHttpRequest(4000);
-    counter = 0;
-    int i = 0;
-    while (digitalRead(button4_pin) == HIGH && i < 30) {
-      delay(20);
-      i++;
-    }
   }
-  if (counter == 5000) {
+  if (!otaEnabled || button != 5) {
     goingToSleep();
-  } else {
-    counter++;
   }
+  readButtons();
+  ArduinoOTA.handle();
 }
