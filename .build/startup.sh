83353b338439c92da342d50e0fe88095c3342a75
cheesemarathon <07bridgesb@gmail.com>
2018-11-08 11:56:32 +0000

    Fix echo styling

--- .build/startup.sh
+++ .build/startup.sh
@@ -32,18 +32,18 @@ fi
 cd /opt/hue-emulator
 
 if [ $3 == "true" ]; then
-    echo "Debug set to '$3' Enabling Debug"
+    echo "\033[33m--Debug set to $3, enabling debug--\033[0m"
     sed -i "s|debug = .* #|debug = True # |g" /opt/hue-emulator/HueEmulator3.py
 else
-    echo "Debug set to '$3' Disabling Debug"
+    echo "\033[33m--Debug set to $3, disabling debug--\033[0m"
     sed -i "s|debug = .* #|debug = False # |g" /opt/hue-emulator/HueEmulator3.py
 fi
 
 echo -e "\033[32m--Startup complete. Open Hue app and search for bridges--\033[0m"
 if [ -z $1 -a -z $2 ]; then
-    echo "Starting without provided MAC & IP"
+    echo "\033[32m--Starting without provided MAC & IP--\033[0m"
     exec python3 /opt/hue-emulator/HueEmulator3.py
 else
-    echo "Starting with provided MAC & IP"
+    echo "\033[32m--Starting with provided MAC & IP--\033[0m"
     exec python3 /opt/hue-emulator/HueEmulator3.py $1 $2
 fi
