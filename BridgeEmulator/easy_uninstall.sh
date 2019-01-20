b193348a10cc0cd683e6871f9b842963f9d53ffd
cheesemarathon <07bridgesb@gmail.com>
2019-01-20 16:40:56 +0000

    Uninstall dependencies last

--- BridgeEmulator/easy_uninstall.sh
+++ BridgeEmulator/easy_uninstall.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 cd /tmp
 
-### Uninstalling dependencies
-echo -e "\033[36m Uninstalling dependencies.\033[0m"
-apt remove -y unzip nmap python3 python3-requests python3-ws4py python3-setuptools && apt autoremove -y
-
-### installing astral library for sunrise/sunset routines
+### Uninstalling astral library for sunrise/sunset routines
 echo -e "\033[36m Uninstalling Python Astral.\033[0m"
 wget -q https://github.com/sffjunkie/astral/archive/master.zip -O astral.zip
 unzip -q -o astral.zip
@@ -16,13 +12,17 @@ tr '\n' '\0' < files.txt | xargs -0 sudo rm -f --
 cd ../
 rm -rf astral.zip astral-master/
 
-### installing hue emulator
+### Uninstalling hue emulator
 echo -e "\033[36m Uninstalling Hue Emulator.\033[0m"
 
 rm -rf /opt/hue-emulator/
-rm /lib/systemd/system/hue-emulator.service
 
-systemctl daemon-reload
 systemctl disable hue-emulator.service
+rm /lib/systemd/system/hue-emulator.service
+systemctl daemon-reload
+
+### Uninstalling dependencies
+echo -e "\033[36m Uninstalling dependencies.\033[0m"
+apt remove -y unzip nmap python3 python3-requests python3-ws4py python3-setuptools && apt autoremove -y
 
 echo -e "\033[32m Uninstal complete!\033[0m"
