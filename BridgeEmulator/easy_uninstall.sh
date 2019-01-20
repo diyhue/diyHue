79a81f87f19f9f47fa067242c476856ab460c556
cheesemarathon <07bridgesb@gmail.com>
2019-01-20 17:42:40 +0000

    Fix read command

--- BridgeEmulator/easy_uninstall.sh
+++ BridgeEmulator/easy_uninstall.sh
@@ -30,7 +30,8 @@ echo -e "\033[36m These are unzip, nmap, python3, python3-requests, python3-ws4p
 echo -e "\033[36m Uninstalling these may break other services that may use them!\033[0m"
 UserInput=""
 while [[ "$UserInput" != "Yes" && "$UserInput" != "No" ]] ; do
-    read -p $UserInput "Would you like to uninsall diyHue dependencies? [Yes/No]"
+    read -p Input "Would you like to uninsall diyHue dependencies? [Yes/No]"
+    $UserInput = $Input
 done
 
 if [ "$UserInput" == "Yes"]
