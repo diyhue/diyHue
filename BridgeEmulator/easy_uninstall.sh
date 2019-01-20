9d011290275f808ea12654dd2d822a51b94cacaa
cheesemarathon <07bridgesb@gmail.com>
2019-01-20 16:46:21 +0000

    Leave dependencies installed

--- BridgeEmulator/easy_uninstall.sh
+++ BridgeEmulator/easy_uninstall.sh
@@ -2,7 +2,7 @@
 cd /tmp
 
 echo -e "\033[36m Stopping diyHue.\033[0m"
-systemctl start hue-emulator.service
+systemctl stop hue-emulator.service
 
 ### Uninstalling astral library for sunrise/sunset routines
 echo -e "\033[36m Uninstalling Python Astral.\033[0m"
@@ -25,7 +25,7 @@ rm /lib/systemd/system/hue-emulator.service
 systemctl daemon-reload
 
 ### Uninstalling dependencies
-echo -e "\033[36m Uninstalling dependencies.\033[0m"
-apt remove -y unzip nmap python3 python3-requests python3-ws4py python3-setuptools && apt autoremove -y
+#echo -e "\033[36m Uninstalling dependencies.\033[0m"
+#apt remove -y unzip nmap python3 python3-requests python3-ws4py python3-setuptools && apt autoremove -y
 
 echo -e "\033[32m Uninstal complete!\033[0m"
