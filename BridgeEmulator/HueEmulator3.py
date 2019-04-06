12a1c11d62cf7eccae7b2d0b0d01a346ca1db965
Motea Marius <marius.motea@gmail.com>
2019-04-06 23:06:44 +0300

    Fix Tradfri Motion Sensor uniqueid bug

--- BridgeEmulator/HueEmulator3.py
+++ BridgeEmulator/HueEmulator3.py
@@ -223,9 +223,9 @@ def addHueMotionSensor(uniqueid, name="Hue motion sensor"):
     if uniqueid == "":
         uniqueid = "00:17:88:01:02:"
         if len(new_sensor_id) == 1:
-            uniqueid += "0" + new_sensor_id + ":"
+            uniqueid += "0" + new_sensor_id
         else:
-            uniqueid += new_sensor_id + ":"
+            uniqueid += new_sensor_id
     bridge_config["sensors"][nextFreeId(bridge_config, "sensors")] = {"name": "Hue temperature sensor 1", "uniqueid": uniqueid + ":d0:5b-02-0402", "type": "ZLLTemperature", "swversion": "6.1.0.18912", "state": {"temperature": None, "lastupdated": "none"}, "manufacturername": "Philips", "config": {"on": False, "battery": 100, "reachable": True, "alert":"none", "ledindication": False, "usertest": False, "pending": []}, "modelid": "SML001"}
     motion_sensor = nextFreeId(bridge_config, "sensors")
     bridge_config["sensors"][motion_sensor] = {"name": name, "uniqueid": uniqueid + ":d0:5b-02-0406", "type": "ZLLPresence", "swversion": "6.1.0.18912", "state": {"lastupdated": "none", "presence": None}, "manufacturername": "Philips", "config": {"on": False,"battery": 100,"reachable": True, "alert": "lselect", "ledindication": False, "usertest": False, "sensitivity": 2, "sensitivitymax": 2,"pending": []}, "modelid": "SML001"}
