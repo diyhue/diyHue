e556c0854db143991b7f9418c9854e1b58458e17
Motea Marius <marius.motea@gmail.com>
2019-07-17 12:24:51 +0300

    Rename config.json into default-config.json

--- BridgeEmulator/easy_install.sh
+++ BridgeEmulator/easy_install.sh
@@ -83,7 +83,7 @@ else
                 exit 1
         fi
         mkdir /opt/hue-emulator
-        cp config.json /opt/hue-emulator/
+        cp default-config.json /opt/hue-emulator/
 
 	curl https://raw.githubusercontent.com/diyhue/diyHue/9ceed19b4211aa85a90fac9ea6d45cfeb746c9dd/BridgeEmulator/openssl.conf -o openssl.conf
 	serial="${mac:0:2}${mac:3:2}${mac:6:2}fffe${mac:9:2}${mac:12:2}${mac:15:2}"
