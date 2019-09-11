5e9a219b3c1613d5bebf823aacb51459550168ab
juanesf <juanesf91@gmail.com>
2019-09-11 12:37:14 -0300

    fix get local ip on OpenWrt

--- BridgeEmulator/functions/network_OpenWrt.py
+++ BridgeEmulator/functions/network_OpenWrt.py
@@ -1,6 +1,34 @@
-import logging
+import os
 import socket
-import sys
+
+if os.name != "nt":
+    import fcntl
+    import struct
+
+    def get_interface_ip(ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915, struct.pack('256s',
+                                ifname[:15]))[20:24])
 
 def getIpAddress():
-   return "192.168.8.1"
+    ip = socket.gethostbyname(socket.gethostname())
+    if ip.startswith("127.") and os.name != "nt":
+        interfaces = [
+            "br-lan",
+            "eth0",
+            "eth1",
+            "eth2",
+            "wlan0",
+            "wlan1",
+            "wifi0",
+            "ath0",
+            "ath1",
+            "ppp0",
+            ]
+        for ifname in interfaces:
+            try:
+                ip = get_interface_ip(ifname)
+                break
+            except IOError:
+                pass
+    return ip
