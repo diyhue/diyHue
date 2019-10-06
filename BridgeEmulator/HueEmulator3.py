14577c370bb4150d179a6ec59c5780b1142355c5
Motea Marius <marius.motea@gmail.com>
2019-10-06 18:11:53 +0300

    add remote discover service

--- BridgeEmulator/HueEmulator3.py
+++ BridgeEmulator/HueEmulator3.py
@@ -1768,6 +1768,7 @@ if __name__ == "__main__":
         Thread(target=daylightSensor).start()
         if dontBlameDiyHue:
             Thread(target=remoteApi, args=[bridge_config["config"]]).start()
+        Thread(target=remoteDiscover, args=[bridge_config["config"]]).start()
 
         while True:
             sleep(10)
