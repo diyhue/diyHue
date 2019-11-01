d25990653af244ea23248e67b8b233f1b15316f9
Motea Marius <marius.motea@gmail.com>
2019-11-01 11:59:05 +0200

    added the new api keys

--- BridgeEmulator/functions/__init__.py
+++ BridgeEmulator/functions/__init__.py
@@ -22,19 +22,21 @@ light_types["ESPHOME-Toggle"]["state"] = {"on": False, "alert": "none", "reachab
 light_types["ESPHOME-Toggle"]["config"] = {"archetype": "classicbulb", "function": "mixed", "direction": "omnidirectional"}
 
 light_types["LCT015"] = {"type": "Extended color light", "swversion": "1.46.13_r26312", "manufacturername": "Philips"}
-light_types["LCT015"]["state"] = {"on": False, "bri": 200, "hue": 0, "sat": 0, "xy": [0.0, 0.0], "ct": 461, "alert": "none", "effect": "none", "colormode": "ct", "reachable": True}
+light_types["LCT015"]["state"] = {"on": False, "bri": 200, "hue": 0, "sat": 0, "xy": [0.0, 0.0], "ct": 461, "alert": "none", "mode": "homeautomation", "effect": "none", "colormode": "ct", "reachable": True}
 light_types["LCT015"]["config"] = {"archetype": "sultanbulb", "function": "mixed", "direction": "omnidirectional"}
-light_types["LCT015"]["capabilities"] = {"certified": True, "control": {"mindimlevel": 1000, "maxlumen": 806, "ct":{"min": 153, "max": 500}}, "streaming":{"renderer": True, "proxy": True}}
+light_types["LCT015"]["capabilities"] = {"certified": True,"control": {"colorgamut": [[0.6915,0.3083],[0.17,0.7],[0.1532,0.0475]],"colorgamuttype": "C","ct": {"max": 500,"min": 153},"maxlumen": 800,"mindimlevel": 1000},"streaming": {"proxy": False,"renderer": True}}
 
 light_types["LST002"] = {"type": "Color light", "swversion": "5.127.1.26581"}
-light_types["LST002"]["state"] = {"on": False, "bri": 200, "hue": 0, "sat": 0, "xy": [0.0, 0.0], "ct": 461, "alert": "none", "effect": "none", "colormode": "ct", "reachable": True}
+light_types["LST002"]["state"] = {"on": False, "bri": 200, "hue": 0, "sat": 0, "xy": [0.0, 0.0], "ct": 461, "alert": "none", "mode": "homeautomation", "effect": "none", "colormode": "ct", "reachable": True}
+light_types["LST002"]["capabilities"] = {"certified": True,"control": {"colorgamut": [[0.704,0.296],[0.2151,0.7106],[0.138,0.08]],"colorgamuttype": "A","maxlumen": 200,"mindimlevel": 10000},"streaming": {"proxy": False,"renderer": True}}
 
 light_types["LWB010"] = {"type": "Dimmable light", "swversion": "1.46.13_r26312", "manufacturername": "Philips"}
 light_types["LWB010"]["state"] = {"on": False, "bri": 254,"alert": "none", "reachable": True}
 light_types["LWB010"]["config"] = {"archetype": "classicbulb", "function": "mixed", "direction": "omnidirectional"}
 
 light_types["LTW001"] = {"type": "Color temperature light", "swversion": "1.46.13_r26312"}
-light_types["LTW001"]["state"] = {"on": False, "colormode": "ct", "alert": "none", "reachable": True, "bri": 254, "ct": 230}
+light_types["LTW001"]["state"] = {"on": False, "colormode": "ct", "alert": "none", "mode": "homeautomation", "reachable": True, "bri": 254, "ct": 230}
+light_types["LTW001"]["capabilities"] = {"certified": True,"control": {"mindimlevel": 1000,"maxlumen": 806,"ct": {"min": 153,"max": 454}},"streaming": {"renderer": False,"proxy": False}}
 
 light_types["Plug 01"] = {"type": "On/Off plug-in unit", "swversion": "V1.04.12"}
 light_types["Plug 01"]["state"] = {"on": False, "alert": "none", "reachable": True}
