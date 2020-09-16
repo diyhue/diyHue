8a3a59da096deba0dbf5f6962616c96f525c944d
root <root@Hue.marius.local>
2020-09-16 09:39:36 +0000

    add configManager

---
+++ BridgeEmulator/configManager/configHandler.py
@@ -0,0 +1,69 @@
+from configManager import configInit
+import datetime
+import os
+import json
+import logManager
+
+logging = logManager.logger.get_logger(__name__)
+
+def _open_json(path):
+    with open(path, 'r', encoding="utf-8") as fp:
+        return json.load(fp)
+
+
+def _write_json(path, contents):
+    with open(path, 'w', encoding="utf-8") as fp:
+        json.dump(contents, fp, sort_keys=True, indent=4, separators=(',', ': '))
+
+
+class Config:
+    json_config = None
+    projectDir = '/opt/hue-emulator'  # cwd = os.path.split(os.path.abspath(__file__))[0]
+    configDir = projectDir + '/config'
+
+    def __init__(self):
+        if not os.path.exists(self.configDir):
+            os.makedirs(self.configDir)
+
+    def load_config(self):
+        try:
+            if os.path.exists(self.configDir + "/config.json"):
+                self.json_config = _open_json(self.configDir + "/config.json")
+                logging.info("Config loaded")
+            else:
+                logging.info("Config not found, creating new config from default settings")
+                self.json_config = _open_json(self.projectDir + '/default-config.json')
+                self.save_config()
+        except Exception:
+            logging.exception("CRITICAL! Config file was not loaded")
+            raise SystemExit("CRITICAL! Config file was not loaded")
+
+    def save_config(self, backup=False):
+        if backup:
+            filename = "config--backup-" + datetime.now().strftime("%Y-%m-%d--%H-%m-%S") + ".json"
+        else:
+            filename = "config.json"
+        path = self.configDir + '/' + filename
+        _write_json(path, self.json_config)
+        return filename
+
+    def reset_config(self):
+        backup = self.save_config(True)
+        try:
+            os.remove(self.configDir + "/config.json")
+        except:
+            logging.exception("Something went wrong when deleting the config")
+        self.load_config()
+        return backup
+
+    def write_args(self, args):
+        self.json_config = configInit.write_args(args, self.json_config)
+
+    def generate_security_key(self):
+        self.json_config = configInit.generate_security_key(self.json_config)
+
+    def sanitizeBridgeScenes(self):
+        self.json_config = configInit.sanitizeBridgeScenes(self.json_config)
+
+    def updateConfig(self):
+        self.json_config = configInit.updateConfig(self.json_config)
---
+++ BridgeEmulator/configManager/runtimeConfigHandler.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from configManager.argumentHandler import parse_arguments
+
+
+@dataclass
+class Config:
+    newLights: dict = None
+    dxState: dict = None
+    arg: dict = None
+
+    def populate(self):
+        self.newLights = {}
+        self.dxState = {"sensors": {}, "lights": {}, "groups": {}}
+        self.arg = parse_arguments()
