0cce2347c95bdb799450af54b956e252c7c7ccec
Alex <alexyao2015@gmail.com>
2020-11-24 19:03:18 -0500

    Provide constants

--- src/core/devices/GenericProtocol.py
+++ src/core/devices/GenericProtocol.py
@@ -5,40 +5,52 @@ from typing import Tuple
 class BaseDeviceProtocol(ABC):
 
     @abstractmethod
+    def supported_devices(self):
+        """
+        Returns dictionary of supported device types
+        return {
+            const.PROTOCOL_BULB_RGB,
+            const.PROTOCOL_BULB_RGBW
+        }
+        :return: dict
+        """
+        raise NotImplementedError()
+
+    @abstractmethod
     def discover(self):
-        raise NotImplementedError("Method must be overridden!")
+        raise NotImplementedError()
 
     @abstractmethod
     def turn_on(self, bri, kelvin, red, green, blue) -> None:
-        raise NotImplementedError("Method must be overridden!")
+        raise NotImplementedError()
 
     @abstractmethod
     def turn_off(self) -> None:
-        raise NotImplementedError("Method must be overridden!")
+        raise NotImplementedError()
 
     @abstractmethod
     def get_is_on(self) -> bool:
-        raise NotImplementedError("Method must be overridden!")
+        raise NotImplementedError()
 
     @abstractmethod
     def on_loop(self):
-        raise NotImplementedError("Method must be overridden!")
+        raise NotImplementedError()
 
 class LightProtocol(BaseDeviceProtocol, ABC):
 
     @abstractmethod
     def set_brightness(self, bri) -> None:
-        raise NotImplementedError("Method must be overridden!")
+        raise NotImplementedError()
 
     @abstractmethod
     def get_brightness(self) -> int:
-        raise NotImplementedError("Method must be overridden!")
+        raise NotImplementedError()
 
 class rgbLightProtocol(LightProtocol, ABC):
 
     @abstractmethod
     def set_color(self, red, green, blue) -> None:
-        raise NotImplementedError("Method must be overridden!")
+        raise NotImplementedError()
 
     @abstractmethod
     def get_color(self) -> Tuple[int, int, int]:
@@ -46,4 +58,4 @@ class rgbLightProtocol(LightProtocol, ABC):
         Get rgb value of light.
         :return: red, green, blue
         """
-        raise NotImplementedError("Method must be overridden!")
+        raise NotImplementedError()
---
+++ src/core/devices/const.py
@@ -0,0 +1,17 @@
+# Supported Protocol Types
+
+PROTOCOL_BULB_TOGGLEABLE = "toggleable_bulb"
+PROTOCOL_BULB_DIMMABLE = "dimmable_bulb"
+PROTOCOL_BULB_WHITE = "w_bulb"
+PROTOCOL_BULB_WARM_WHITE_COOL_WHITE = "ww_bulb"
+PROTOCOL_BULB_RGB = "rgb_bulb"
+PROTOCOL_BULB_RGBW = "rgbw_bulb"
+PROTOCOL_BULB_RGBWW = "rgbww_bulb"
+
+PROTOCOL_STRIP_TOGGLEABLE = "toggleable_strip"
+PROTOCOL_STRIP_DIMMABLE = "dimmable_strip"
+PROTOCOL_STRIP_WHITE = "w_strip"
+PROTOCOL_STRIP_WARM_WHITE_COOL_WHITE = "ww_strip"
+PROTOCOL_STRIP_RGB = "rgb_strip"
+PROTOCOL_STRIP_RGBW = "rgbw_strip"
+PROTOCOL_STRIP_RGBWW = "rgbww_strip"
