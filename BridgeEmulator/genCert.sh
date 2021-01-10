fd6b86c40b9716bdf5728bf27129af106ca0e628
Motea Marius <marius.motea@gmail.com>
2021-01-11 00:19:16 +0200

    Update genCert.sh

--- BridgeEmulator/genCert.sh
+++ BridgeEmulator/genCert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mac=$1
-dec_serial=`python3 -c "print(int(\"$mac\", 16))"`
+dec_serial=`python3 -c "print(int(\"$mac\".strip('\u200e'), 16))"`
 openssl req -new -days 3650 -config /opt/hue-emulator/openssl.conf  -nodes -x509 -newkey  ec -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve   -subj "/C=NL/O=Philips Hue/CN=$mac" -keyout private.key -out public.crt -set_serial $dec_serial
 
 touch /opt/hue-emulator/cert.pem
