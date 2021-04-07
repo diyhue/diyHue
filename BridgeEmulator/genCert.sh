5fab19164fc64f5784a9c92e482c77c95cc1058d
Marius <marius.motea@gmail.com>
2021-04-07 21:37:17 +0300

    generate cert in the config folder

--- BridgeEmulator/genCert.sh
+++ BridgeEmulator/genCert.sh
@@ -3,11 +3,9 @@ mac=$1
 dec_serial=`python3 -c "print(int(\"$mac\".strip('\u200e'), 16))"`
 openssl req -new -days 3650 -config /opt/hue-emulator/openssl.conf  -nodes -x509 -newkey  ec -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve   -subj "/C=NL/O=Philips Hue/CN=$mac" -keyout private.key -out public.crt -set_serial $dec_serial
 
-touch /opt/hue-emulator/cert.pem
+touch /opt/hue-emulator/config/cert.pem
 
-cat private.key > /opt/hue-emulator/cert.pem
-cat public.crt >> /opt/hue-emulator/cert.pem
-
-cp /opt/hue-emulator/cert.pem /opt/hue-emulator/config/cert.pem
+cat private.key > /opt/hue-emulator/config/cert.pem
+cat public.crt >> /opt/hue-emulator/config/cert.pem
 
 rm private.key public.crt 
