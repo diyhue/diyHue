394dc5b7da5449f61b9c446a3527deb383df31bb
David <fisico994@gmail.com>
2021-04-23 20:03:09 +0200

    [ReactUI] Added linkbutton and notifications (#610)
    * pulled remote
    
    * new background, various fixes
    
    Changes and compressed backgrounds
    Fixed range thumb on FF
    Fixed expanded margin
    Fixed dimmer
    
    * added linkbutton styling
    
    * Added Notifications
    
    * Added transition to sidebar
    
    * linkbtn transition
    
    * fixed typo

--- React/src/views/LinkButton.js
+++ React/src/views/LinkButton.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { FaTimes } from "react-icons/fa";
 
 export default function LinkButton({API_KEY}) {
   console.log(API_KEY)
@@ -13,8 +14,17 @@ export default function LinkButton({API_KEY}) {
 
   return (
     <div className="content">
-      <p>Description for Linkbutton</p>
-      <div className="linkbtn" onClick={() => pushLinkButton()}>Call to action
+       <div className="notificationContainer">
+         <div className="notification error"><p>%Errormessage%</p><div className="icon"><FaTimes/></div></div>
+         <div className="notification warning"><p>%Errormessage%</p><div className="icon"><FaTimes/></div></div>
+         <div className="notification success"><p>%Errormessage%</p><div className="icon"><FaTimes/></div></div>
+       </div>
+
+
+      <div className="contentContainer">
+        <p>Push this button to accept the pairing of the requested app</p>
+        <div className="linkbtn" onClick={() => pushLinkButton()}>Link App
+        <div class="linkbtn2"></div></div>
       </div>
     </div>
   );
