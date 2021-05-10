c2c3d5937970d0372a003f46285b76a765616fe2
Gregory Mallios <44323649+gmallios@users.noreply.github.com>
2021-05-11 00:03:19 +0300

    Update swversion & WLED Support to flask branch (#628)
    * update swversion
    
    * Add WLED to flask branch with entertainment support
    
    * This adds WLED as a v1 device for the time being
    * Adds entertainment support over UDP streaming
    
    * Change WLED modelid to LST002

--- BridgeEmulator/lights/protocols/__init__.py
+++ BridgeEmulator/lights/protocols/__init__.py
@@ -1,3 +1,3 @@
-from lights.protocols import yeelight, tasmota, shelly, mi_box,  native, native_single, native_multi, esphome, mqtt, wiz
+from lights.protocols import wled, yeelight, tasmota, shelly, mi_box,  native, native_single, native_multi, esphome, mqtt, wiz
 
-protocols = [yeelight, tasmota, shelly, mi_box, native, native_single, native_multi, esphome, mqtt, wiz]
+protocols = [wled, yeelight, tasmota, shelly, mi_box, native, native_single, native_multi, esphome, mqtt, wiz]
