83ce5c0147a30804900247d630288acfcee3ba85
Gregory Mallios <gregmallios@gmail.com>
2021-05-11 14:41:37 +0300

    [ReactUI] Various changes
    * Add linkbutton to topbar
    * Fix notification close icon cursor
    
    If linkbutton on topbar is liked we can remove the page and simplify the UI

--- React/src/containers/TheHeader.js
+++ React/src/containers/TheHeader.js
@@ -2,10 +2,13 @@ import { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
 import axios from "axios";
 import logo from '../static/images/logo.svg';
+import Flash from './Flash'
 
 const TheHeader = ({showSidebar, setShowSidebar, API_KEY}) => {
 
   const [group0State, setGroup0State] = useState(false);
+  const [type, setType] = useState('none');
+  const [message, setMessage] = useState('no message');
 
   useEffect(() => {
     fetchGroups();
@@ -26,6 +29,24 @@ const TheHeader = ({showSidebar, setShowSidebar, API_KEY}) => {
     }
   }
 
+  const pushLinkButton = () => {
+    axios
+      .put(
+        `/api/${API_KEY}/config`,
+        {'linkbutton': {'lastlinkbuttonpushed': Date.now() / 1000 | 0 }}
+      ).then((fetchedData) => {
+        console.log(fetchedData.data);
+        setMessage('Pairing is allowed for 30 seconds');
+        setType('none');
+        setType('success');
+      }).catch((error) => {
+        console.error(error)
+        setMessage('Error occured, check browser console');
+        setType('none');
+        setType('error');
+      });
+    };
+
   const handleToggleChange = (state) => {
     const newState = {'on': state};
     setGroup0State(state);
@@ -40,6 +61,10 @@ const TheHeader = ({showSidebar, setShowSidebar, API_KEY}) => {
         <FaBars/>
         <span></span>
       </button>
+      <div className="switchContainer">
+        {type !== 'none' && <Flash type={type} message={message} duration="5000" setType={setType} />}
+        <button className="headerlinkbtn" onClick={() => pushLinkButton()}>Link App</button>
+      </div>
       <div className="switchContainer">
         <p>Turn all {group0State? 'off' : 'on'}</p>
         <label className="switch">
