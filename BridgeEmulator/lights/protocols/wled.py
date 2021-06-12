8f6d1d6df38220025be88ab730fb4af9ceffff20
Gregory Mallios <44323649+gmallios@users.noreply.github.com>
2021-06-13 01:10:02 +0300

    WLED Segments (#648)
    * wled segments
    
    * wled segment support
    
    * Update wled with segment support.
    
    WLED protocol now splits each segment in its own light upon discovery. This breaks current configured WLED lights and thus users need to delete them and re-discover them.
    Entertainment support shouldn't be affected as it doesn't depend on any segment functionality.
    
    * Fix wled state
    
    * Fix various wled bugs
    
    * Added device_ips method for wled discovery.
    
    Co-authored-by: Gregory Mallios <greg@Gregorys-MacBook-Air.local>

--- BridgeEmulator/lights/protocols/wled.py
+++ BridgeEmulator/lights/protocols/wled.py
@@ -5,45 +5,75 @@ import random
 import math
 import sys
 import logManager
+import requests
 from functions.colors import convert_rgb_xy, convert_xy, rgbBrightness
 from time import sleep
 from zeroconf import IPVersion, ServiceBrowser, ServiceStateChange, Zeroconf, ZeroconfServiceTypes
 
 logging = logManager.logger.get_logger(__name__)
 
-mdns = []
+discovered_lights = []
 Connections = {}
 
 
 def on_mdns_discover(zeroconf, service_type, name, state_change):
-    global mdns
+    global discovered_lights
     if "wled" in name and state_change is ServiceStateChange.Added:
         info = zeroconf.get_service_info(service_type, name)
         if info:
             addresses = ["%s" % (socket.inet_ntoa(addr))
                          for addr in info.addresses]
-            mdns.append([addresses[0], name])
+            discovered_lights.append([addresses[0], name])
 
 
-def discover(detectedLights):
-    logging.info('wled discovery started')
+def discover(detectedLights, device_ips):
+    logging.info('<WLED> discovery started')
     ip_version = IPVersion.V4Only
     zeroconf = Zeroconf(ip_version=ip_version)
     services = ["_http._tcp.local."]
     browser = ServiceBrowser(zeroconf, services, handlers=[on_mdns_discover])
     sleep(2)
-    for device in mdns:
+    if len(discovered_lights) == 0:
+        # Didn't find anything using mdns, trying device_ips
+        logging.info("<WLED> Nothing found using mDNS, trying device_ips method...")
+        for ip in device_ips:
+            try:
+                response = requests.get("http://" + ip + "/json/info", timeout=3)
+                if response.status_code == 200:
+                    json_resp = json.loads(response.content)
+                    if json_resp['brand'] == "WLED":
+                        discovered_lights.append([ip, json_resp['name']])
+            except Exception as e:
+                logging.debug("<WLED> ip %s is unknown device", ip)
+
+    lights = []
+    for device in discovered_lights:
         try:
             x = WledDevice(device[0], device[1])
-            logging.info("Found wled: " + device[1])
-            modelid = "LST002"
-            lightName = x.name
-            detectedLights.append({"protocol": "wled", "name": lightName, "modelid": modelid, "protocol_cfg": {
-                              "ip": device[0], "ledCount": x.ledCount, "mdns_name": device[1], "mac": x.mac}})
+            logging.info("<WLED> Found device: %s with %d segments" %
+                         (device[1], x.segmentCount))
+            modelid = "LCX002"  # Gradient Strip
+            segmentid = 0
+            for _ in range(1, x.segmentCount+1):
+                lights.append({"protocol": "wled",
+                               "name": x.name + "_seg" + str(segmentid),
+                               "modelid": modelid,
+                               "protocol_cfg": {
+                                   "ip": x.ip,
+                                   "ledCount": x.ledCount,
+                                   "mdns_name": device[1],
+                                   "mac": x.mac,
+                                   "segmentId": segmentid
+                               }
+                               })
+                segmentid = segmentid + 1
+            for light in lights:
+                detectedLights.append(light)
         except:
             break
 
-def set_light(light, data, rgb):
+
+def set_light(light, data):
     ip = light.protocol_cfg['ip']
     if ip in Connections:
         c = Connections[ip]
@@ -51,44 +81,50 @@ def set_light(light, data, rgb):
         c = WledDevice(ip, light.protocol_cfg['mdns_name'])
         Connections[ip] = c
 
-    state = {}
-    for k, v in data.items():
-        if k == "on":
-            if v:
-                state['on'] = True
+    if "alert" in data:
+        c.setRGBSeg(150, 0, 0,
+                    light.protocol_cfg['segmentId'])
+        sleep(0.2)
+        c.setRGBSeg(0, 0, 150,
+                    light.protocol_cfg['segmentId'])
+        return
+            
+    for _, value in data.items():
+        if _ == "on":
+            if value:
+                c.setOnSeg(True, light.protocol_cfg['segmentId'])
+                return
             else:
-                state['on'] = False
-        elif k == "bri":
-            state['bri'] = v+1
-        elif k == "ct":
-            kelvin = round(translateRange(v, 153, 500, 6500, 2000))
-            color = kelvinToRgb(kelvin)
-            state = {"seg": [{"col": [[color[0], color[1], color[2]]]}]}
-            c.sendJson(state)
-        elif k == "xy":
-            if rgb:
-                color = rgbBrightness(rgb, 255)
-                state = {"seg": [{"col": [[color[0], color[1], color[2]]]}]}
-                c.sendJson(state)
-            else:
-                color = convert_xy(v[0], v[1], 255)
-                state = {"seg": [{"col": [[color[0], color[1], color[2]]]}]}
-        c.sendJson(state)
+                c.setOnSeg(False, light.protocol_cfg['segmentId'])
+                return
+        for __, ivalue in value.items():
+            for k, v in ivalue.items():
+                if k == "on":
+                    if v:
+                        c.setOnSeg(True, light.protocol_cfg['segmentId'])
+                    else:
+                        c.setOnSeg(False, light.protocol_cfg['segmentId'])
+                elif k == "bri":
+                    c.setBriSeg(v+1, light.protocol_cfg['segmentId'])
+                elif k == "ct":
+                    kelvin = round(translateRange(v, 153, 500, 6500, 2000))
+                    color = kelvinToRgb(kelvin)
+                    c.setRGBSeg(color[0], color[1], color[2],
+                                light.protocol_cfg['segmentId'])
+                elif k == "xy":
+                    color = convert_xy(v[0], v[1], 255)
+                    c.setRGBSeg(color[0], color[1], color[2],
+                                light.protocol_cfg['segmentId'])
 
 
-def get_light_state(address, light):
-    state = {}
-    with urllib.request.urlopen('http://' + address['ip'] + '/json/state') as resp:
-        data = json.loads(resp.read())
-        state['on'] = data['on']
-        state['bri'] = data['bri']
-        # Weird division by zero when a color is 0
-        r = int(data['seg'][0]['col'][0][0])+1
-        g = int(data['seg'][0]['col'][1][0])+1
-        b = int(data['seg'][0]['col'][2][0])+1
-        state['xy'] = convert_rgb_xy(r, g, b)
-        state["colormode"] = "xy"
-        return state
+def get_light_state(light):
+    ip = light.protocol_cfg['ip']
+    if ip in Connections:
+        c = Connections[ip]
+    else:
+        c = WledDevice(ip, light.protocol_cfg['mdns_name'])
+        Connections[ip] = c
+    return c.getSegState(light.protocol_cfg['segmentId'])
 
 
 def translateRange(value, leftMin, leftMax, rightMin, rightMax):
@@ -126,23 +162,57 @@ class WledDevice:
         self.url = 'http://' + self.ip
         self.ledCount = 0
         self.mac = None
+        self.segmentCount = 1  # Default number of segments in WLED
+        self.segments = []
         self.getInitialState()
+
+    def getInitialState(self):
+        self.state = self.getLightState()
+        self.getSegments()
+        self.getLedCount()
+        self.getMacAddr()
+
+    def getLedCount(self):
+        self.ledCount = self.state['info']['leds']['count']
+
+    def getMacAddr(self):
+        self.mac = ':'.join(self.state[
+                            'info']['mac'][i:i+2] for i in range(0, 12, 2))
+
+    def getSegments(self):
+        self.segments = self.state['state']['seg']
+        self.segmentCount = len(self.segments)
 
     def getLightState(self):
         with urllib.request.urlopen(self.url + '/json') as resp:
             data = json.loads(resp.read())
             return data
 
-    def getInitialState(self):
-        self.getLedCount()
-        self.getMacAddr()
+    def getSegState(self, seg):
+        state = {}
+        data = self.getLightState()['state']['seg'][seg]
+        state['bri'] = data['bri']
+        state['on'] = data['on']
+        state['bri'] = data['bri']
+        # Weird division by zero when a color is 0
+        r = int(data['col'][0][0])+1
+        g = int(data['col'][0][1])+1
+        b = int(data['col'][0][2])+1
+        state['xy'] = convert_rgb_xy(r, g, b)
+        state["colormode"] = "xy"
+        return state
 
-    def getLedCount(self):
-        self.ledCount = self.getLightState()['info']['leds']['count']
+    def setRGBSeg(self, r, g, b, seg):
+        state = {"seg": [{"id": seg, "col": [[r, g, b]]}]}
+        self.sendJson(state)
 
-    def getMacAddr(self):
-        self.mac = ':'.join(self.getLightState()[
-                            'info']['mac'][i:i+2] for i in range(0, 12, 2))
+    def setOnSeg(self, on, seg):
+        state = {"seg": [{"id": seg, "on": on}]}
+        self.sendJson(state)
+
+    def setBriSeg(self, bri, seg):
+        state = {"seg": [{"id": seg, "bri": bri}]}
+        self.sendJson(state)
 
     def sendJson(self, data):
         req = urllib.request.Request(self.url + "/json")
