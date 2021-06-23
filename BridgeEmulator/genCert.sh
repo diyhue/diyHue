d31d07a6539d1a55442da961a8b6194335a2ed6a
juanesf <juanesf91@gmail.com>
2021-06-23 07:30:47 -0400

    Update install script for openwrt beta branch (#658)
    * Update genCert.sh
    
    --------------------------------------------------------------------------------
    juanesf@CNC:~$ mac=$1
    juanesf@CNC:~$ dec_serial=`python3 -c "print(int(\"$mac\".strip('\u200e'), 16))"`
    Traceback (most recent call last):
      File "<string>", line 1, in <module>
    ValueError: invalid literal for int() with base 16: ''
    
    --------------------------------------------------------------------------------
    juanesf@CNC:~$ mac=`cat /sys/class/net/$(ip route get 8.8.8.8 | sed -n 's/.* dev \([^ ]*\).*/\1/p')/address`
    juanesf@CNC:~$ echo $mac
    00:15:5d:00:90:da
    juanesf@CNC:~$ serial="${mac:0:2}${mac:3:2}${mac:6:2}fffe${mac:9:2}${mac:12:2}${mac:15:2}"
    juanesf@CNC:~$ echo $serial
    00155dfffe0090da
    juanesf@CNC:~$ dec_serial=`python3 -c "print(int(\"$serial\", 16))"`
    juanesf@CNC:~$ echo $dec_serial
    6014328570417370
    
    --------------------------------------------------------------------------------
    
    * Update easy_openwrt.sh
    
    - Correct folder structure in beta branch
    - Add dependencies for new interface based on flask
    
    * Update easy_openwrt.sh
    
    - Remove jsonify in the installation, since it comes integrated with flask
    
    * Update docker-release.yml
    
    - To test warnings
    
    * Revert "Update docker-release.yml"
    
    This reverts commit 523300150e47de2247ee5899abbe03e12f354955.

--- BridgeEmulator/genCert.sh
+++ BridgeEmulator/genCert.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-mac=$1
-dec_serial=`python3 -c "print(int(\"$mac\".strip('\u200e'), 16))"`
+mac=`cat /sys/class/net/$(ip route get 8.8.8.8 | sed -n 's/.* dev \([^ ]*\).*/\1/p')/address`
+serial="${mac:0:2}${mac:3:2}${mac:6:2}fffe${mac:9:2}${mac:12:2}${mac:15:2}"
+dec_serial=`python3 -c "print(int(\"$serial\", 16))"`
 openssl req -new -days 3650 -config /opt/hue-emulator/openssl.conf  -nodes -x509 -newkey  ec -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve   -subj "/C=NL/O=Philips Hue/CN=$mac" -keyout private.key -out public.crt -set_serial $dec_serial
 
 touch /opt/hue-emulator/config/cert.pem
