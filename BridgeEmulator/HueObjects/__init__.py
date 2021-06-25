1f6ff4d05e9ba361baff69e0f41927033ca8814c
Marius <marius.motea@gmail.com>
2021-06-25 11:47:06 +0300

    fix deepcopy

--- BridgeEmulator/HueObjects/__init__.py
+++ BridgeEmulator/HueObjects/__init__.py
@@ -135,9 +135,9 @@ class Light():
         self.id_v1 = data["id_v1"]
         self.id_v2 = data["id_v2"] if "id_v2" in data else genV2Uuid()
         self.uniqueid = data["uniqueid"] if "uniqueid" in data else generate_unique_id()
-        self.state = data["state"] if "state" in data else lightTypes[self.modelid]["state"].deepcopy()
+        self.state = data["state"] if "state" in data else lightTypes[self.modelid]["state"].copy.deepcopy()
         self.protocol = data["protocol"] if "protocol" in data else "dummy"
-        self.config = data["config"] if "config" in data else lightTypes[self.modelid]["config"].deepcopy()
+        self.config = data["config"] if "config" in data else lightTypes[self.modelid]["config"].copy.deepcopy()
         self.protocol_cfg = data["protocol_cfg"] if "protocol_cfg" in data else {}
         self.streaming = False
 
@@ -954,9 +954,9 @@ class Sensor():
             if "manufacturername" not in data:
                 data["manufacturername"] = sensorTypes[data["modelid"]][data["type"]]["static"]["manufacturername"]
             if "config" not in data:
-                data["config"] = sensorTypes[data["modelid"]][data["type"]]["config"].deepcopy()
+                data["config"] = sensorTypes[data["modelid"]][data["type"]]["config"].copy.deepcopy()
             if "state" not in data:
-                data["state"] = sensorTypes[data["modelid"]][data["type"]]["state"].deepcopy()
+                data["state"] = sensorTypes[data["modelid"]][data["type"]]["state"].copy.deepcopy()
             if "swversion" not in data:
                 data["swversion"] = sensorTypes[data["modelid"]][data["type"]]["static"]["swversion"]
         if "config" not in data:
