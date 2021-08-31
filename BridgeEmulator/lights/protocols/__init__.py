4d2b2ab41a836df89c9f8c0d4adc5ee94555d879
hdhdu <43926658+hdhdu@users.noreply.github.com>
2021-08-31 11:36:18 +0200

    Fixed milight protocol and added it to the __init__.py. Changed all renamed config keys and fixed bugs in code. (#690)
    Co-authored-by: Markus Fiebig <m.fiebig@logic-joe.com>

--- BridgeEmulator/lights/protocols/__init__.py
+++ BridgeEmulator/lights/protocols/__init__.py
@@ -1,3 +1,3 @@
-from lights.protocols import wled, hyperion, yeelight, tasmota, shelly, mi_box, hue, deconz, tradfri, native, native_single, native_multi, esphome, mqtt, wiz
+from lights.protocols import wled, hyperion, yeelight, tasmota, shelly, mi_box, hue, deconz, tradfri, native, native_single, native_multi, esphome, mqtt, wiz, milight
 
-protocols = [wled, hyperion, yeelight, tasmota, shelly, mi_box, hue, deconz, tradfri, native, native_single, native_multi, esphome, mqtt, wiz]
+protocols = [wled, hyperion, yeelight, tasmota, shelly, mi_box, hue, deconz, tradfri, native, native_single, native_multi, esphome, mqtt, wiz, milight]
