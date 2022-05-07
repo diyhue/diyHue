8e6a740d2d9269e57cd2db90de701f0b0d352d30
Motea Marius <marius.motea@gmail.com>
2022-05-07 19:45:08 +0300

    Update install.sh

--- BridgeEmulator/install.sh
+++ BridgeEmulator/install.sh
@@ -98,7 +98,7 @@ esac
 echo -e "\033[36m Installing dependencies.\033[0m"
 if type apt &> /dev/null; then
   # Debian-based distro
-  apt-get install -y unzip python3 python3-pip openssl
+  apt-get install -y unzip python3 python3-pip openssl bluez bluetooth
 elif type pacman &> /dev/null; then
   # Arch linux
   pacman -Syq --noconfirm || exit 1
