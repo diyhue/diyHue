5337aa3c9e2b275515986147d4e24509a1ad0048
Marius <marius.motea@gmail.com>
2022-06-05 10:50:51 +0300

    new web interface release

---
+++ BridgeEmulator/flaskUI/static/js/19.708b683c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpdiyhue=this.webpackJsonpdiyhue||[]).push([[19],{217:function(e,s,n){"use strict";n.r(s);var i=n(7);s.default=function(){return Object(i.jsx)("div",{className:"inner",children:Object(i.jsxs)("div",{className:"contentContainer",children:[Object(i.jsx)("div",{className:"headline",children:"Settings"}),Object(i.jsx)("p",{children:"Work in progress. "})]})})}}}]);
+//# sourceMappingURL=19.708b683c.chunk.js.map
