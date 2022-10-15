d9f46794e33f0c1d99e6715e3027887c8036f4b2
juanesf <juanesf91@gmail.com>
2022-10-15 11:09:33 -0300

    Update genCert.sh
    - Add folder creation after touch cert.pem

--- BridgeEmulator/genCert.sh
+++ BridgeEmulator/genCert.sh
@@ -3,6 +3,7 @@ mac=$1
 dec_serial=`python3 -c "print(int(\"$mac\".strip('\u200e'), 16))"`
 openssl req -new -days 3650 -config /opt/hue-emulator/openssl.conf  -nodes -x509 -newkey  ec -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve   -subj "/C=NL/O=Philips Hue/CN=$mac" -keyout private.key -out public.crt -set_serial $dec_serial
 
+mkdir -p /opt/hue-emulator/config
 touch /opt/hue-emulator/config/cert.pem
 
 cat private.key > "$2/cert.pem"
