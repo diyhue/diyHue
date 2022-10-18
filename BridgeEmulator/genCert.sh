c3686e82e14f878a7a377431b933e396c250794f
Austin Drummond <adrum0x7@gmail.com>
2022-10-18 04:27:25 -0400

    Fix config path in genCert.sh (#865)
    Fixes #963

--- BridgeEmulator/genCert.sh
+++ BridgeEmulator/genCert.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 mac=$1
+config="${2:-/opt/hue-emulator/config}"
 dec_serial=`python3 -c "print(int(\"$mac\".strip('\u200e'), 16))"`
 openssl req -new -days 3650 -config /opt/hue-emulator/openssl.conf  -nodes -x509 -newkey  ec -pkeyopt ec_paramgen_curve:P-256 -pkeyopt ec_param_enc:named_curve   -subj "/C=NL/O=Philips Hue/CN=$mac" -keyout private.key -out public.crt -set_serial $dec_serial
 
-mkdir -p /opt/hue-emulator/config
-touch /opt/hue-emulator/config/cert.pem
+mkdir -p $config
+touch $config/cert.pem
 
-cat private.key > "$2/cert.pem"
-cat public.crt >> "$2/cert.pem"
+cat private.key > "$config/cert.pem"
+cat public.crt >> "$config/cert.pem"
 
 rm private.key public.crt
