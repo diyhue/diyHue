078c4d99047694896205e828cb5b1ad465a54df3
Alexander Falkenstern <alexander.falkenstern@gmail.com>
2024-02-22 21:57:09 +0100

    Don't overwrite already known lamp types.

--- BridgeEmulator/lights/protocols/hue.py
+++ BridgeEmulator/lights/protocols/hue.py
@@ -1,3 +1,4 @@
+from lights.light_types import lightTypes
 import json
 import logManager
 import requests
@@ -5,7 +6,8 @@ import requests
 logging = logManager.logger.get_logger(__name__)
 
 def set_light(light, data):
-    url = "http://" + light.protocol_cfg["ip"] + "/api/" + light.protocol_cfg["hueUser"] + "/lights/" + light.protocol_cfg["id"] + "/state"
+    protocol_cfg = light.protocol_cfg
+    url = "http://" + protocol_cfg["ip"] + "/api/" + protocol_cfg["hueUser"] + "/lights/" + protocol_cfg["id"] + "/state"
     payload = {}
     payload.update(data)
     color = {}
@@ -22,30 +24,45 @@ def set_light(light, data):
         color["sat"] = payload["sat"]
         del(payload["sat"])
     if len(payload) != 0:
-        requests.put(url, json=payload, timeout=3)
+        response = requests.put(url, json=payload, timeout=3)
+        logging.debug(response.text)
     if len(color) != 0:
-        requests.put(url, json=color, timeout=3)
+        response = requests.put(url, json=color, timeout=3)
+        logging.debug(response.text)
 
 def get_light_state(light):
-    state = requests.get("http://" + light.protocol_cfg["ip"] + "/api/" + light.protocol_cfg["hueUser"] + "/lights/" + light.protocol_cfg["id"], timeout=3)
+    protocol_cfg = light.protocol_cfg
+    state = requests.get("http://" + protocol_cfg["ip"] + "/api/" + protocol_cfg["hueUser"] + "/lights/" + protocol_cfg["id"], timeout=3)
     return state.json()["state"]
 
 def discover(detectedLights, credentials):
     if "hueUser" in credentials and len(credentials["hueUser"]) > 32:
         logging.debug("hue: <discover> invoked!")
         try:
-            response = requests.get("http://" + credentials["ip"] + "/api/" + credentials["hueUser"] + "/lights", timeout=3)
+            ip = credentials['ip']
+            user = credentials['hueUser']
+            response = requests.get("http://" + ip + "/api/" + user + "/lights", timeout=3)
             if response.status_code == 200:
                 logging.debug(response.text)
                 lights = json.loads(response.text)
                 for id, light in lights.items():
-                    modelid = "LCT015"
-                    if light["type"] == "Dimmable light":
-                        modelid = "LWB010"
-                    elif light["type"] == "Color temperature light":
-                        modelid = "LTW001"
-                    elif light["type"] == "On/Off plug-in unit":
-                        modelid = "LOM001"
-                    detectedLights.append({"protocol": "hue", "name": light["name"], "modelid": modelid, "protocol_cfg": {"ip": credentials["ip"], "hueUser": credentials["hueUser"], "modelid": light["modelid"], "id": id, "uniqueid": light["uniqueid"]}})
+                    if ('modelid' in light) and (light['modelid'] in lightTypes):
+                        modelid = light['modelid']
+ #                   elif ('productid' in light) : 
+ #                       productid = light['productid'] --> Philips-LCG002-3-GU10ECLv2
+                    elif light['type'] == 'Extended color light':
+                        modelid = 'LCG002'
+                    elif light['type'] == 'Color temperature light':
+                        modelid = 'LTW001'
+                    elif light['type'] == 'Dimmable light':
+                        modelid = 'LWB010'
+                    elif light['type'] == 'On/Off plug-in unit':
+                        modelid = 'LOM001'
+                    else:
+                        modelid = 'LCT001'
+                    detectedLights.append({
+                        "protocol": "hue", "name": light["name"], "modelid": modelid,
+                        "protocol_cfg": {"ip": ip, "hueUser": user, "modelid": light['modelid'], "id": id, "uniqueid": light["uniqueid"]}
+                    })
         except Exception as e:
             logging.info("Error connecting to Hue Bridge: %s", e)
