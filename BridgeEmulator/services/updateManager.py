18148dba4eb99ad6e50fadab23e0fb9eaeb18b6d
Mark <hendriksen-mark@hotmail.com>
2024-02-25 20:26:15 +0100

    Update updateManager.py

--- BridgeEmulator/services/updateManager.py
+++ BridgeEmulator/services/updateManager.py
@@ -2,7 +2,8 @@ import requests
 import configManager
 import logManager
 import json
-from datetime import datetime
+from datetime import datetime, timezone
+import subprocess
 
 bridgeConfig = configManager.bridgeConfig.yaml_config
 logging = logManager.logger.get_logger(__name__)
@@ -28,4 +29,34 @@ def versionCheck():
             logging.info("no swversion number update")
 
 def githubCheck():
-    logging.debug("work in process")
+    #creation_time = 2024-02-18 19:50:15.000000000 +0100
+    creation_time = subprocess.run("stat -c %y HueEmulator3.py", shell=True, capture_output=True, text=True)
+    creation_time = creation_time.stdout.replace("\n", "")
+    creation_time_arg1 = creation_time.split(" ")
+    creation_time_arg2 = creation_time_arg1[1].split(".")
+    creation_time = creation_time_arg1[0] + " " + creation_time_arg2[0] + " " + creation_time_arg1[2]
+    creation_time = datetime.strptime(creation_time, "%Y-%m-%d %H:%M:%S %z")
+    creation_time = creation_time.astimezone(timezone.utc)
+    creation_time = creation_time.strftime("%Y-%m-%d %H")
+
+    url = "https://api.github.com/repos/diyhue/diyhue/branches/master"
+    response = requests.get(url)
+    if response.status_code == 200:
+        device_data = json.loads(response.text)
+        publish_time = device_data["commit"]["commit"]["author"]["date"]
+        publish_time = datetime.strptime(publish_time, "%Y-%m-%dT%H:%M:%SZ")
+        publish_time = publish_time.strftime("%Y-%m-%d %H")
+
+    logging.info("creation_time strftime: " + str(creation_time))
+    logging.info("publish_time          : " + str(publish_time))
+
+    if publish_time > creation_time:
+        logging.info("update on github")
+        bridgeConfig["config"]["swupdate2"]["state"] = "allreadytoinstall"
+        bridgeConfig["config"]["swupdate2"]["bridge"]["state"] = "allreadytoinstall"
+    else:
+        logging.info("no update on github")
+        bridgeConfig["config"]["swupdate2"]["state"] = "noupdates"
+        bridgeConfig["config"]["swupdate2"]["bridge"]["state"] = "noupdates"
+
+    bridgeConfig["config"]["swupdate2"]["checkforupdate"] = False
