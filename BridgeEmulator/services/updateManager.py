69fce95ddd3b1103b04c9df8cd1cc97bcd7c7925
Mark <hendriksen-mark@hotmail.com>
2024-02-25 21:08:26 +0100

    Update updateManager.py

--- BridgeEmulator/services/updateManager.py
+++ BridgeEmulator/services/updateManager.py
@@ -47,16 +47,47 @@ def githubCheck():
         publish_time = datetime.strptime(publish_time, "%Y-%m-%dT%H:%M:%SZ")
         publish_time = publish_time.strftime("%Y-%m-%d %H")
 
-    logging.info("creation_time strftime: " + str(creation_time))
-    logging.info("publish_time          : " + str(publish_time))
+    logging.info("creation_time diyHue : " + str(creation_time))
+    logging.info("publish_time  diyHue : " + str(publish_time))
 
     if publish_time > creation_time:
         logging.info("update on github")
         bridgeConfig["config"]["swupdate2"]["state"] = "allreadytoinstall"
         bridgeConfig["config"]["swupdate2"]["bridge"]["state"] = "allreadytoinstall"
+    elif githubUICheck() == True:
+        logging.info("UI update on github")
+        bridgeConfig["config"]["swupdate2"]["state"] = "anyreadytoinstall"
+        bridgeConfig["config"]["swupdate2"]["bridge"]["state"] = "anyreadytoinstall"
     else:
-        logging.info("no update on github")
+        logging.info("no update for diyHue or UI on github")
         bridgeConfig["config"]["swupdate2"]["state"] = "noupdates"
         bridgeConfig["config"]["swupdate2"]["bridge"]["state"] = "noupdates"
 
     bridgeConfig["config"]["swupdate2"]["checkforupdate"] = False
+
+def githubUICheck():
+    #creation_time = 2024-02-18 19:50:15.000000000 +0100
+    creation_time = subprocess.run("stat -c %y flaskUI/templates/index.html", shell=True, capture_output=True, text=True)
+    creation_time = creation_time.stdout.replace("\n", "")
+    creation_time_arg1 = creation_time.split(" ")
+    creation_time_arg2 = creation_time_arg1[1].split(".")
+    creation_time = creation_time_arg1[0] + " " + creation_time_arg2[0] + " " + creation_time_arg1[2]
+    creation_time = datetime.strptime(creation_time, "%Y-%m-%d %H:%M:%S %z")
+    creation_time = creation_time.astimezone(timezone.utc)
+    creation_time = creation_time.strftime("%Y-%m-%d %H")
+
+    url = "https://api.github.com/repos/diyhue/diyhueUI/branches/master"
+    response = requests.get(url)
+    if response.status_code == 200:
+        device_data = json.loads(response.text)
+        publish_time = device_data["commit"]["commit"]["author"]["date"]
+        publish_time = datetime.strptime(publish_time, "%Y-%m-%dT%H:%M:%SZ")
+        publish_time = publish_time.strftime("%Y-%m-%d %H")
+
+    logging.info("creation_time UI : " + str(creation_time))
+    logging.info("publish_time  UI : " + str(publish_time))
+
+    if publish_time > creation_time:
+        return True
+    else:
+        return False
