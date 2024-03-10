561b773bbebeef832b823ed420d2147c7d84a64e
Mark <hendriksen-mark@hotmail.com>
2024-03-10 12:58:45 +0100

    Update githubUIInstall.sh

--- BridgeEmulator/githubUIInstall.sh
+++ BridgeEmulator/githubUIInstall.sh
@@ -1,12 +1,13 @@
 curl -s localhost/save
 #cd /
-if [ ! -d diyhueUI ]; then
- mkdir diyhueUI
+if [ -d diyhueUI ]; then
+ rm -r diyhueUI
 fi
+mkdir diyhueUI
 curl -sL https://github.com/diyhue/diyHueUI/releases/latest/download/DiyHueUI-release.zip -o diyHueUI.zip
 unzip -qo diyHueUI.zip -d diyhueUI
 #cd diyhueUI
-mv diyhueUI/index.html /opt/hue-emulator/flaskUI/templates/
+cp -r diyhueUI/index.html /opt/hue-emulator/flaskUI/templates/
 cp -r diyhueUI/static/ /opt/hue-emulator/flaskUI/static/
 
 curl -s localhost/restart
