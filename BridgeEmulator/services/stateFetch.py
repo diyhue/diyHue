e550aa02ad6d574c39345695e6599d938837d0c9
Mark <hendriksen-mark@hotmail.com>
2024-04-15 00:26:27 +0200

    Update stateFetch.py

--- BridgeEmulator/services/stateFetch.py
+++ BridgeEmulator/services/stateFetch.py
@@ -33,7 +33,7 @@ def syncWithLights(off_if_unreachable): #update Hue Bridge lights states
                 lu = user.last_use_date
                 try: #in case if last use is not a proper datetime
                     lu = datetime.strptime(lu, "%Y-%m-%dT%H:%M:%S")
-                    if abs(datetime.now(timezone.utc) - lu) <= timedelta(seconds = 2):
+                    if abs(datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%S") - lu) <= timedelta(seconds = 2):
                         i = 300
                         break
                 except:
